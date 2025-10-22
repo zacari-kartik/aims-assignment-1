acfe9d756bbd1c232b1c2637af59576fbcbc620a
zacari-kartik <2k07.kartikprajapati@gmail.com>
2025-10-23 01:10:41 +0530

    Update one _hot_encoding.py

--- one _hot_encoding.py
+++ one _hot_encoding.py
@@ -1,5 +1,5 @@
 import pandas as pd
-poke = {'evolution': ['charmander', 'charmeleon', 'charizard', 'mega charizard x', 'mega charizard y', 'charmeleon', 'charizard']}
+poke = {'evolution': ['charmander', 'charmeleon', 'charizard', 'mega charizard x', 'mega charizard y']}
 df = pd.DataFrame(poke)
 one_hot = pd.get_dummies(df['evolution'])
 print(one_hot
