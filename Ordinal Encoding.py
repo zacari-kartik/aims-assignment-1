acd89a6022741956d069924f67705fc27df7f0b7
zacari-kartik <2k07.kartikprajapati@gmail.com>
2025-10-22 22:00:31 +0530

    Update Ordinal Encoding.py

--- Ordinal Encoding.py
+++ Ordinal Encoding.py
@@ -1,5 +1,7 @@
 import pandas as pd
 poke = {'evolution': ['charmander', 'charmeleon', 'charizard', 'mega charizard x', 'mega charizard y', 'charmeleon', 'charizard']}
 df = pd.DataFrame(poke)
+print(df)
 evolution_order = {'charmander': 1, 'charmeleon': 2, 'charizard': 3, 'mega charizard x': 4, 'mega charizard y': 5}
 df['evolution_encoded'] = df['evolution'].map(evolution_order)
+print(df)
