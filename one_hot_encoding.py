e55d40e942c6e3c446f14d81f82e674bd7c4e2be
zacari-kartik <2k07.kartikprajapati@gmail.com>
2025-10-23 19:16:43 +0530

    Update one_hot_encoding.py

--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import numpy as np
 poke = {'evolution': ['charmander', 'charmeleon', 'charizard', 'mega charizard x', 'mega charizard y', 'charizard', 'charmander']}
 df = pd.DataFrame(poke)
-one_hot = pd.get_dummies(df['evolution'])
-print(one_hot)
+print (df)
+#here i used the help of ai to get what i was  thinking to code
+unique_categories = df['evolution'].unique()
+one_hot_df = pd.DataFrame()
+
+for category in unique_categories:
+  #if the row in df['evolution'] equals the category, put 1 else put 0
+  one_hot_df[category] = np.where(df['evolution'] == category, 1, 0)
+
+print (one_hot_df)
